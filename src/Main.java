c025926138069c19407a33cbcfa13c44e618e166
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-01-31 14:44:09 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener listenerError = System.out::println;
+
+        Worker worker = new Worker(listener, listenerError);
+        worker.start();
+
     }
 }
